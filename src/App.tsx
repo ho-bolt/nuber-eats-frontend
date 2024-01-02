7a1d9fb825e855d0e5dc443f5066484b8c61749b
ho-bolt <gbolt@naver.com>
2024-01-02 17:27:18 +0900

    ✨Feat : 아폴로 세팅
    - 아폴로 사용하기 위한 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,16 @@
+import React from 'react'
 import { LoggedOutRouter } from './routers/logged-out-router'
+import { gql, useQuery } from '@apollo/client'
+
+const IS_LOGGED_IN = gql`
+  query isLoggedIn {
+    isLoggedIn @client
+  }
+`
 
 function App() {
+  const { data } = useQuery(IS_LOGGED_IN)
+  console.log(data)
   return <LoggedOutRouter />
 }
 
