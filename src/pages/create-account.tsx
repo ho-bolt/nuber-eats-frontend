fb6e9a1b42f7c26f50eaf08e71ae5ddc586173f1
ho-bolt <gbolt@naver.com>
2024-01-09 09:35:39 +0900

    ✨Feat : 로고 추가 및 컴포넌트 추가
    - 로그인 페이지와 계정생성 페이지에 표시할 로고 추가
    - 계정생성 컴포넌트 추가
    -  로그인 컴포넌트 수정

--- src/pages/create-account.tsx
+++ src/pages/create-account.tsx
@@ -1,3 +1,167 @@
+import React from 'react'
+import { useForm } from 'react-hook-form'
+import { FormError } from '../components/form-error'
+import { gql, useMutation } from '@apollo/client'
+import {
+  CreateAccountMutation,
+  CreateAccountMutationVariables,
+  UserRole,
+} from '../__generated__/graphql'
+import nuberLogo from '../images/uber-eats-logosvg.svg'
+import { Button } from '../components/button'
+import { Link } from 'react-router-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { get } from 'http'
+import { error } from 'console'
+import { useHistory } from 'react-router-dom'
+export const CREATE_ACCOUNT_MUTATION = gql`
+  mutation createAccount($createAccountInput: CreateAccountInput!) {
+    createAccount(input: $createAccountInput) {
+      ok
+      error
+    }
+  }
+`
+
+interface ICreateAccountForm {
+  email: string
+  password: string
+  role: UserRole
+}
+
 export const CreateAccount = () => {
-  return <div>create account</div>
+  const {
+    register,
+    getValues,
+    watch,
+    formState: { errors, isValid },
+    handleSubmit,
+  } = useForm<ICreateAccountForm>({
+    mode: 'onChange',
+    defaultValues: {
+      role: UserRole.Client,
+    },
+  })
+  // 9분 36초
+
+  // loading == mutation 실행중,
+  // error == mutation이 error 반환
+  // onCompleted는 login이 있다는 것을 의미한다.
+
+  /* useMutation은 array를 return하는데
+     첫 원소는 반드시 호출해줘야 하는 mutation function이다. 
+     두번째 원소는 object이다. 
+   */
+  const history = useHistory()
+  const onCompleted = (data: CreateAccountMutation) => {
+    const {
+      createAccount: { ok, error },
+    } = data
+    // 계정 생성 완료되면 로그인 페이지로 리다이렉트
+    if (ok) {
+      history.push('/login')
+    }
+  }
+  const [
+    createAccountMutation,
+    { loading, data: createAccountMutationResult },
+  ] = useMutation<CreateAccountMutation, CreateAccountMutationVariables>(
+    CREATE_ACCOUNT_MUTATION,
+    {
+      onCompleted,
+    },
+  )
+
+  const onSubmit = () => {
+    if (!loading) {
+      const { email, password, role } = getValues()
+      createAccountMutation({
+        variables: {
+          createAccountInput: {
+            email,
+            password,
+            role,
+          },
+        },
+      })
+    }
+  }
+  // tailwind-css lg == large screen 이다.  즉 큰 화면일 땐 margin-top을 28주고 디폴트는 10 준다.
+  // css를 할 때 항상 모바일 먼저 해야하고 기본 class name은 항상 모바일로 시작해야 한다.
+  // react-helmet은 document의 head를 변경할 수 있게 해준다.
+  return (
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <HelmetProvider>
+        <Helmet>
+          <title>Create Account | Nuber </title>
+        </Helmet>
+      </HelmetProvider>
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={nuberLogo} className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-3xl mb-5">
+          Let's get started
+        </h4>
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="grid gap-3 mt-5 w-full mb-5"
+        >
+          <input
+            {...register('email', {
+              required: 'Email is required',
+              pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            })}
+            name="email"
+            type="email"
+            placeholder="Email"
+            className="input"
+          />
+          {errors.email?.message && (
+            <FormError errorMessage={errors.email.message} />
+          )}
+          {errors.email?.type === 'pattern' && (
+            <FormError errorMessage={'Please enter a valid email'} />
+          )}
+          <input
+            {...register('password', {
+              required: 'Password is required',
+              minLength: {
+                value: 3,
+                message: 'Password must be more than 8 chars',
+              },
+            })}
+            name="password"
+            type="password"
+            placeholder="Password"
+            className="input"
+          />
+          {errors.password?.message && (
+            <FormError errorMessage={errors.password?.message} />
+          )}
+
+          <select name="role" ref={register('role').ref} className="input">
+            {Object.keys(UserRole).map((role, index) => (
+              <option key={index}>{role}</option>
+            ))}
+          </select>
+
+          <Button
+            canClick={isValid}
+            loading={loading}
+            actionTest={'Create Account'}
+          />
+          {createAccountMutationResult?.createAccount.error && (
+            <FormError
+              errorMessage={createAccountMutationResult.createAccount.error}
+            />
+          )}
+        </form>
+        <div>
+          Already have an account?{' '}
+          <Link to="/login" className=" text-black font-bold hover:underline">
+            Log In Now
+          </Link>
+        </div>
+      </div>
+    </div>
+  )
 }
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,12 +2,12 @@ import React from 'react'
 import { useForm } from 'react-hook-form'
 import { FormError } from '../components/form-error'
 import { ApolloError, gql, useMutation } from '@apollo/client'
-import {
-  LoginInput,
-  LoginMutation,
-  LoginMutationVariables,
-} from '../__generated__/graphql'
-
+import { LoginMutation, LoginMutationVariables } from '../__generated__/graphql'
+import nuberLogo from '../images/uber-eats-logosvg.svg'
+import { Button } from '../components/button'
+import { Link } from 'react-router-dom'
+import { Helmet, HelmetProvider } from 'react-helmet-async'
+import { isLoggedInVar } from '../apollo'
 export const LOGIN_MUTATION = gql`
   mutation login($loginInput: LoginInput!) {
     login(input: $loginInput) {
@@ -27,9 +27,11 @@ export const Login = () => {
   const {
     register,
     getValues,
-    formState: { errors },
+    formState: { errors, isValid },
     handleSubmit,
-  } = useForm<ILoginForm>()
+  } = useForm<ILoginForm>({
+    mode: 'onChange',
+  })
 
   // loading == mutation 실행중,
   // error == mutation이 error 반환
@@ -40,18 +42,19 @@ export const Login = () => {
       login: { ok, token },
     } = data
     if (ok) {
-      console.log(token)
+      isLoggedInVar(true)
     }
   }
-  const onError = (error: ApolloError) => {
-    console.log(error.graphQLErrors)
-  }
+
+  /* useMutation은 array를 return하는데
+     첫 원소는 반드시 호출해줘야 하는 mutation function이다. 
+     두번째 원소는 object이다. 
+   */
   const [loginMutation, { data: loginMutationResult, loading }] = useMutation<
     LoginMutation,
     LoginMutationVariables
   >(LOGIN_MUTATION, {
     onCompleted,
-    onError,
   })
 
   const onSubmit = () => {
@@ -66,25 +69,41 @@ export const Login = () => {
         },
       })
     }
-    loginMutation()
   }
+  // tailwind-css lg == large screen 이다.  즉 큰 화면일 땐 margin-top을 28주고 디폴트는 10 준다.
+  // css를 할 때 항상 모바일 먼저 해야하고 기본 class name은 항상 모바일로 시작해야 한다.
+  // react-helmet은 document의 head를 변경할 수 있게 해준다.
   return (
-    <div className="h-screen flex items-center justify-center bg-gray-800">
-      <div className="bg-white w-full max-w-lg pt-10 pb-7  rounded-lg text-center">
-        <h3 className="font-bold text-lg text-gray-800">Log In </h3>
+    <div className="h-screen flex items-center flex-col mt-10 lg:mt-28">
+      <HelmetProvider>
+        <Helmet>
+          <title>Login | Nuber </title>
+        </Helmet>
+      </HelmetProvider>
+      <div className="w-full max-w-screen-sm flex flex-col px-5 items-center">
+        <img src={nuberLogo} className="w-52 mb-10" />
+        <h4 className="w-full font-medium text-left text-3xl mb-5">
+          Welcome back
+        </h4>
         <form
           onSubmit={handleSubmit(onSubmit)}
-          className="grid  gap-3 mt-5 px-5"
+          className="grid gap-3 mt-5 w-full mb-5"
         >
           <input
-            {...register('email', { required: 'Email is required' })}
+            {...register('email', {
+              required: 'Email is required',
+              pattern: /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            })}
             name="email"
             type="email"
             placeholder="Email"
-            className="input mb-3"
+            className="input"
           />
           {errors.email?.message && (
             <FormError errorMessage={errors.email.message} />
+          )}
+          {errors.email?.type === 'pattern' && (
+            <FormError errorMessage={'Please enter a vaild email'} />
           )}
           <input
             {...register('password', {
@@ -102,13 +121,20 @@ export const Login = () => {
           {errors.password?.message && (
             <FormError errorMessage={errors.password?.message} />
           )}
-          <button className="btn mt-3">
-            {loading ? 'Loading...' : 'Log In'}
-          </button>
+          <Button canClick={isValid} loading={loading} actionTest={'Log In'} />
           {loginMutationResult?.login.error && (
             <FormError errorMessage={loginMutationResult.login.error} />
           )}
         </form>
+        <div>
+          New to Nuber?{' '}
+          <Link
+            to="/create-account"
+            className=" text-black font-bold hover:underline"
+          >
+            Create an Account
+          </Link>
+        </div>
       </div>
     </div>
   )
