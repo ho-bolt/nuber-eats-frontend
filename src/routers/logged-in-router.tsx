cf7851479d7056377748db239a66acb86b65fdba
ho-bolt <gbolt@naver.com>
2024-01-10 17:11:54 +0900

    ✨Feat : verifyEmail and useME
     - 자주 쓰는 로그인한 유저 확인 hook으로 따로 뺌

--- src/routers/logged-in-router.tsx
+++ src/routers/logged-in-router.tsx
@@ -1,32 +1,28 @@
 import React from 'react'
-import { gql, useQuery } from '@apollo/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Switch } from 'react-router-dom'
 import { Route } from 'react-router-dom'
 import { Restaurants } from '../pages/client/restaurants'
 import { NotFound } from '../pages/404'
+import { Header } from '../components/header'
+import { useMe } from '../hooks/useMe'
+import { Redirect } from 'react-router-dom'
+import { ConfirmEmail } from '../pages/user/confirm-email'
 
 const ClientRoutes = () => (
   <>
     <Route path="/" exact>
       <Restaurants />
     </Route>
+    <Route path="/confirm" exact>
+      <ConfirmEmail />
+    </Route>
   </>
 )
 
-const ME_QUERY = gql`
-  query meQuery {
-    me {
-      id
-      email
-      role
-      verified
-    }
-  }
-`
-
 export const LoggedInRouter = () => {
-  const { data, loading, error } = useQuery(ME_QUERY)
+  const { data, loading, error } = useMe()
+  // 여기서 처음 호출
   if (!data || loading || error) {
     return (
       <div className="h-screen flex justify-center items-center">
@@ -36,9 +32,9 @@ export const LoggedInRouter = () => {
   }
   return (
     <BrowserRouter>
+      <Header />
       <Switch>
         {data.me.role === 'Client' && <ClientRoutes />}
-
         <Route>
           <NotFound />
         </Route>
