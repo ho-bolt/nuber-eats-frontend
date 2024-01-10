a15cbe317defc07a00f96bbcdf8e72d3cf622339
ho-bolt <gbolt@naver.com>
2024-01-10 17:23:40 +0900

    Reapply "✨Feat : verifyEmail and useME"
    This reverts commit 6b742c29424ef6b460b920f8fefbe6f4f900a278.

---
+++ src/hooks/useMe.tsx
@@ -0,0 +1,21 @@
+import { gql, useQuery } from '@apollo/client'
+import { MeQuery } from '../__generated__/graphql'
+
+const ME_QUERY = gql`
+  query me {
+    me {
+      id
+      email
+      role
+      verified
+    }
+  }
+`
+
+export const useMe = () => {
+  return useQuery<MeQuery>(ME_QUERY)
+}
+
+// 이걸 hook으로 만든 이유는 apollo가 cache에서 가져올 수 있기 때문이다.
+// useMe 같은 경우는 모든 페이지마다 내가 누군지 호출해야하기 때문에 hook으로 만들고
+// cache에서 내 정보를 그때그때 가져올 수 있다.
